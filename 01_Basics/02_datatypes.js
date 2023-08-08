c809c87e6e9231cc45575a9d3040c9b634dfbd8c
vivektenali <vivektenali@gmail.com>
2023-08-08 23:24:20 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treats all JS code as a newer version
+
+//alert() we are using nodejs not a browser
+
+let name = "vivek";
+let age = 12;
+let isLoggedin = true;
+let state;
+let value = null;
+
+console.log([
+  typeof name,
+  typeof age,
+  typeof isLoggedin,
+  typeof state,
+  typeof value,
+]);
+
+//typeof null is object
+
+//Primitive data
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//Non-primitive data
+//Array
+//Object
