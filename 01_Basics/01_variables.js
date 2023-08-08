3dae10e36b175a9fbae17afe35d62f86eb84c19f
vivektenali <vivektenali@gmail.com>
2023-08-08 23:16:38 +0530

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = "123";
+let accountEmail = "xyz@gmail.com";
+var accountPassword = "123";
+accountCity = "Hyd";
+let accountState;
+
+console.log([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
