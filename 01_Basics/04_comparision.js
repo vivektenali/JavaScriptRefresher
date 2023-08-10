6398d363e25ef9f0ae2eac888b694c4915305ffe
vivektenali <vivektenali@gmail.com>
2023-08-10 22:26:44 +0530

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict type check
+
+console.log("2" === 2);
