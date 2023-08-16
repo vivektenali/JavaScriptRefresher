1e6c112714b8bcfda33eb965d0373ecd750eb21f
vivektenali <vivektenali@gmail.com>
2023-08-16 18:13:42 +0530

    numbers

---
+++ 01_Basics/07_numbers_and_maths.js
@@ -0,0 +1,11 @@
+const score = 100;
+console.log(score);
+
+const myScore = new Number(100);
+console.log(typeof myScore);
+
+console.log(myScore.toString().length);
+console.log(myScore.toFixed(2));
+
+const hundreds = 1000000000;
+console.log(hundreds.toLocaleString("en-IN"));
