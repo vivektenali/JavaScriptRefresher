ef32c177f3cad21f36dd84b452d5fe36732ea332
vivektenali <vivektenali@gmail.com>
2023-08-21 16:31:43 +0530

    added few methods

--- 02_Basics/02_arrays_two.js
+++ 02_Basics/02_arrays_two.js
@@ -22,3 +22,38 @@ let score1 = 100;
 let score2 = 200;
 let score3 = 300;
 console.log(Array.of(score1, score2, score3));
+
+/* Array Methods */
+/*
+
+toString();
+join();
+concat();
+splice();
+slice();
+indexOf();
+lastindexof();
+flat();
+forEach();
+
+
+
+Good to know
+
+map();
+filter();
+reduce();
+some();
+every();
+find();
+findIndex();
+sort();
+
+
+
+
+
+
+
+
+*/
