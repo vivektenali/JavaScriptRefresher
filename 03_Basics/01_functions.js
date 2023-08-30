8b240300a1fb4a9768f522d4d4fe11fede07cec3
vivektenali <vivektenali@gmail.com>
2023-08-30 22:42:59 +0530

    Functions Intro

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,35 @@
+function sayMyName() {
+  console.log("V");
+  console.log("I");
+  console.log("V");
+  console.log("E");
+  console.log("K");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2) {
+  // let result = number1 + number2
+  // return result
+  return number1 + number2;
+}
+
+const result = addTwoNumbers(3, 5);
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "test") {
+  if (!username) {
+    console.log("PLease enter a username");
+    return;
+  }
+  return `${username} just logged in`;
+}
+
+// console.log(loginUserMessage("vivek"))
+//console.log(loginUserMessage(""));
