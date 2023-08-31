d1cbef9f71e86a08dc536ec6aba0e4e20c58e988
vivektenali <vivektenali@gmail.com>
2023-08-31 22:50:56 +0530

    Intro to Scope

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,15 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+var c = 300;
+let a = 100;
+if (true) {
+  let a = 10;
+  const b = 20;
+  var c = 30;
+}
+
+console.log(a);
+// console.log(b);
+//console.log(c);
